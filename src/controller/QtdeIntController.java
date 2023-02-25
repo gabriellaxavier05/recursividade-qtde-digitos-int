0d20fa3ceb2b0d97ca114b937eabf0d3c5f60f37
Gabriella Xavier <gabriellaxaviersp05@outlook.com>
2023-02-25 20:40:16 -0300

    Comentário de explicação na classe controller

--- src/controller/QtdeIntController.java
+++ src/controller/QtdeIntController.java
@@ -9,6 +9,11 @@ public class QtdeIntController {
 		}
 		else {
 			return 1 + qtdeDigInt(n / 10);
+			/*
+			 * se n for maior q 10, o valor vai ser dividido por 10. o resultado dessa divisão será passado como argumento
+			 * quando a função for chamada novamente. Pra cada divisão realizada, é somado 1, q é como se fosse o contador.
+			 * Quando n (o resultado da divisão) torna-se menor q 10, a recursividade para e o tamanho do número informado é retornado.
+			 */
 		}
 	}
 }
